8f27e41ea2b1f1723cddc352e13c88a9cc93941e
Said-Abdikarim <kareemfrmto@gmail.com>
2019-10-05 16:48:40 -0400

    Added comments for the prof

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+//Add the UI object instance to the app.js.
 const interface = new UI();
 
+//Event listener for encoding
 interface.btn_en.addEventListener("click", () => {
     interface.encodeMsg(interface.textA.value);
 })
 
+//Event listener for decoding
 interface.btn_dec.addEventListener("click", () => {
     interface.decodeMsg(interface.textA.value);
 })
